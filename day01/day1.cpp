58065952c9b809ce7b8943471c0cd3c41a4f50cb
FluffyGameDev <fluffygamedev@gmail.com>
2022-11-30 22:23:52 +0100

    Initial commit

---
+++ day01/day1.cpp
@@ -0,0 +1,40 @@
+﻿#include <algorithm>
+#include <fstream>
+#include <numeric>
+#include <vector>
+
+#include <fmt/core.h>
+
+bool ReadInputValues(/*TODO: output data*/)
+{
+    static const char* inputFile{ "input.txt" };
+    std::ifstream inputStream{ inputFile };
+
+    bool readSucceeded{ inputStream.is_open() };
+    if (readSucceeded)
+    {
+        while (!inputStream.eof())
+        {
+            std::uint32_t readValue{};
+            inputStream >> readValue; //TODO: use some other struct
+        }
+
+        inputStream.close();
+    }
+
+    return readSucceeded;
+}
+
+int main()
+{
+    if (ReadInputValues())
+    {
+        //TODO
+    }
+    else
+    {
+        fmt::print("Failed to open input file.\n");
+    }
+
+    return 0;
+}
