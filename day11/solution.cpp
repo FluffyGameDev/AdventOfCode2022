ecb9e5efa88b9cff5bf43da3fbdf45084009d806
FluffyGameDev <71411509+FluffyGameDev@users.noreply.github.com>
2022-12-11 17:22:51 +0100

    Day 11 Progress - Input Parsing

--- day11/solution.cpp
+++ day11/solution.cpp
@@ -3,6 +3,8 @@
 
 #include "bytecode.h"
 #include <numeric>
+#include <sstream>
+#include <string>
 
 namespace AoC
 {
@@ -30,6 +32,7 @@ namespace AoC
 
             for (u32 itemValue : currentMonkey.HeldItems)
             {
+                interpreter.Reset();
                 interpreter.SetRegisterA(itemValue);
                 interpreter.RunBytecode(currentMonkeyData.BytecodeBuffer);
                 itemValue = interpreter.GetRegisterA() / 3;
@@ -64,153 +67,92 @@ namespace AoC
             return std::accumulate(countersCopy.begin(), nthIterator, 1U,
                 [](u32 total, u32 value) { return total * value; });
         }
+
+        void CompileBytecode(const std::string& inputLine, std::vector<u8>& bytecodeBuffer)
+        {
+            std::stringstream operationStream{ inputLine };
+            std::string operand1Text{};
+            std::string operand2Text{};
+            std::string operatorText{};
+            std::string ignore{};
+            operationStream >> ignore >> ignore >> ignore >> operand1Text >> operatorText >> operand2Text;
+            if (operand1Text == "old")
+            {
+                bytecodeBuffer.push_back((u8)Bytecode::PushRegA);
+            }
+            else
+            {
+                bytecodeBuffer.push_back((u8)Bytecode::PushConst);
+                u32 value{ (u32)std::atoi(operand1Text.c_str()) };
+                char* valueCharBuffer{ reinterpret_cast<char*>(&value) };
+                for (u32 i = 0; i < sizeof(u32); ++i)
+                {
+                    bytecodeBuffer.push_back((u8)valueCharBuffer[i]);
+                }
+            }
+
+            if (operand2Text == "old")
+            {
+                bytecodeBuffer.push_back((u8)Bytecode::PushRegA);
+            }
+            else
+            {
+                bytecodeBuffer.push_back((u8)Bytecode::PushConst);
+                u32 value{ (u32)std::atoi(operand2Text.c_str()) };
+                char* valueCharBuffer{ reinterpret_cast<char*>(&value) };
+                for (u32 i = 0; i < sizeof(u32); ++i)
+                {
+                    bytecodeBuffer.push_back((u8)valueCharBuffer[i]);
+                }
+            }
+
+            if (operatorText == "*") { bytecodeBuffer.push_back((u8)Bytecode::Mul); }
+            else if (operatorText == "+") { bytecodeBuffer.push_back((u8)Bytecode::Add); }
+
+            bytecodeBuffer.push_back((u8)Bytecode::PopRegA);
+        }
     }
 
     bool ReadInput(std::istream& inputStream, InputData& inputData, AoCStep step)
     {
-        if (step == AoCStep::Test)
+        std::string line{};
+        std::string ignore{};
+        while (!inputStream.eof())
         {
-            inputData.Monkeys =
+            MonkeyData data{};
+            std::getline(inputStream, line);
+
+            //Starting items: ???
+            u32 itemId{};
+            std::getline(inputStream, line);
+            std::stringstream itemsStream{ line };
+            itemsStream >> ignore >> ignore;
+            while (itemsStream >> itemId)
             {
-                {
-                    { 79, 98 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)19, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Mul, (u8)Bytecode::PopRegA },
-                    23, 2, 3
-                },
-                {
-                    { 54, 65, 75, 74 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)6, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Add, (u8)Bytecode::PopRegA },
-                    19, 2, 0
-                },
-                {
-                    { 79, 60, 97 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushRegA, (u8)Bytecode::Mul, (u8)Bytecode::PopRegA },
-                    13, 1, 3
-                },
-                {
-                    { 74 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)3, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Add, (u8)Bytecode::PopRegA },
-                    17, 0, 1
-                }
-            };
+                data.StartingItems.push_back(itemId);
+                itemsStream >> ignore;
+            }
+
+            //Operation: new = ???
+            std::getline(inputStream, line);
+            Internal::CompileBytecode(line, data.BytecodeBuffer);
+
+            //Test: divisible by ???
+            std::getline(inputStream, line);
+            sscanf_s(line.c_str(), "  Test: divisible by %d", &data.DivisibilityCondition);
+
+            //If true: throw to monkey ???
+            std::getline(inputStream, line);
+            sscanf_s(line.c_str(), "    If true: throw to monkey %d", &data.ConditionTrueMonkey);
+
+            //If false: throw to monkey ???
+            std::getline(inputStream, line);
+            sscanf_s(line.c_str(), "    If false: throw to monkey %d", &data.ConditionFalseMonkey);
+
+            inputData.Monkeys.push_back(data);
+            std::getline(inputStream, line);
         }
-        else if (step == AoCStep::RealData)
-        {
-            inputData.Monkeys =
-            {
-                /*
-                Monkey 0:
-                  Starting items: 83, 62, 93
-                  Operation: new = old * 17
-                  Test: divisible by 2
-                    If true: throw to monkey 1
-                    If false: throw to monkey 6
-                */
-                {
-                    { 83, 62, 93 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)17, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Mul, (u8)Bytecode::PopRegA },
-                    2, 1, 6
-                },
 
-                /*
-                Monkey 1:
-                  Starting items: 90, 55
-                  Operation: new = old + 1
-                  Test: divisible by 17
-                    If true: throw to monkey 6
-                    If false: throw to monkey 3
-                */
-                {
-                    { 90, 55 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)1, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Add, (u8)Bytecode::PopRegA },
-                    17, 6, 3
-                },
-
-                /*
-                Monkey 2:
-                  Starting items: 91, 78, 80, 97, 79, 88
-                  Operation: new = old + 3
-                  Test: divisible by 19
-                    If true: throw to monkey 7
-                    If false: throw to monkey 5
-                */
-                {
-                    { 91, 78, 80, 97, 79, 88 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)3, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Add, (u8)Bytecode::PopRegA },
-                    19, 7, 5
-                },
-
-                /*
-                Monkey 3:
-                  Starting items: 64, 80, 83, 89, 59
-                  Operation: new = old + 5
-                  Test: divisible by 3
-                    If true: throw to monkey 7
-                    If false: throw to monkey 2
-                */
-                {
-                    { 64, 80, 83, 89, 59 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)5, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Add, (u8)Bytecode::PopRegA },
-                    3, 7, 2
-                },
-
-                /*
-                Monkey 4:
-                  Starting items: 98, 92, 99, 51
-                  Operation: new = old * old
-                  Test: divisible by 5
-                    If true: throw to monkey 0
-                    If false: throw to monkey 1
-                */
-                {
-                    { 98, 92, 99, 51 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushRegA, (u8)Bytecode::Mul, (u8)Bytecode::PopRegA },
-                    5, 0, 1
-                },
-
-                /*
-                Monkey 5:
-                  Starting items: 68, 57, 95, 85, 98, 75, 98, 75
-                  Operation: new = old + 2
-                  Test: divisible by 13
-                    If true: throw to monkey 4
-                    If false: throw to monkey 0
-                */
-                {
-                    { 68, 57, 95, 85, 98, 75, 98, 75 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)2, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Add, (u8)Bytecode::PopRegA },
-                    13, 4, 0
-                },
-
-                /*
-                Monkey 6:
-                  Starting items: 74
-                  Operation: new = old + 4
-                  Test: divisible by 7
-                    If true: throw to monkey 3
-                    If false: throw to monkey 2
-                */
-                {
-                    { 74 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)4, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Add, (u8)Bytecode::PopRegA },
-                    7, 3, 2
-                },
-
-                /*
-                Monkey 7:
-                  Starting items: 68, 64, 60, 68, 87, 80, 82
-                  Operation: new = old * 19
-                  Test: divisible by 11
-                    If true: throw to monkey 4
-                    If false: throw to monkey 5
-                */
-                {
-                    { 68, 64, 60, 68, 87, 80, 82 },
-                    { (u8)Bytecode::PushRegA, (u8)Bytecode::PushConst, (u8)19, (u8)0, (u8)0, (u8)0, (u8)Bytecode::Mul, (u8)Bytecode::PopRegA },
-                    11, 4, 5
-                }
-            };
-        }
         return true;
     }
 
